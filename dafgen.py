3d15c0f1474e5f5d9a7235dc6c6c2a603a83023f
MaxiToy2000 <passiondev227@gmail.com>
2026-04-26 05:02:18 -0400

    commit 11 conflict

--- dafgen.py
+++ dafgen.py
@@ -144,6 +144,7 @@ if __name__ == '__main__':
 
 ####### dev_commit1
 
+<<<<<<< HEAD
 ####### work2_commit8 how to pull
 
 ####### work2_commit10
@@ -157,3 +158,7 @@ if __name__ == '__main__':
 ####### work_commit5
 
 ####### work_commit11
+=======
+####### work2_commit7 how to pull
+
+>>>>>>> fcc317a (commit9_after pull.rebase)
